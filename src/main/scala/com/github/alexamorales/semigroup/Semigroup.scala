841efcf798b220e4a7d387eaa0b8336e248592f6
Oleksii Lisun <oleksii.lisun@idemia.com>
2019-06-03 10:09:27 +0200

    Chapter 3 - functor first exercise

--- src/main/scala/com/github/alexamorales/semigroup/Semigroup.scala
+++ src/main/scala/com/github/alexamorales/semigroup/Semigroup.scala
@@ -11,5 +11,4 @@ trait Monoid[A] extends Semigroup[A] {
 object Monoid {
   def apply[A](implicit monoid: Monoid[A]): Monoid[A] =
     monoid
-
 }
---
+++ src/test/scala/com/github/alexamorales/functor/TreeFunctorSpec.scala
@@ -0,0 +1,22 @@
+package com.github.alexamorales.functor
+import org.scalatest.{Matchers, WordSpec}
+
+class TreeFunctorSpec extends WordSpec with Matchers {
+
+
+  "A Tree functor" should {
+    import Tree._
+    import Tree.Instances._
+    import cats.syntax.functor._
+
+    "map over leaf" in {
+      leaf(10).map(_ + 10) should be(leaf(20))
+    }
+
+    "map over tree" in {
+      val tree = branch(branch(leaf(10), leaf(20)), leaf(30))
+      val expected = branch(branch(leaf(20), leaf(30)), leaf(40))
+      tree.map(_ + 10) should be(expected)
+    }
+  }
+}
